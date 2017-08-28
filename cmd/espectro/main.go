c0332b834b5397951f4137a1828dae0038b5f6c2
Daniel Olano <danielolanoc@gmail.com>
2017-08-28 18:04:47 +0200

    Add flag to print version

--- cmd/espectro/main.go
+++ cmd/espectro/main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"flag"
 	"log"
-	"github.com/olanod/espectro"
 	"os"
 	"time"
+
+	"github.com/olanod/espectro"
 )
 
-var (
-	n, sampleRate *uint
-	interval *time.Duration
-)
-
-func init() {
-	n = flag.Uint("n", 10, "Distribute output spectrum in n")
-	sampleRate = flag.Uint("r", 44100, "Sample rate")
-	interval = flag.Duration("i", 50 * time.Millisecond, "Window size")
-	flag.Parse()
-}
+var version string
 
 func main() {
+	n := flag.Uint("n", 10, "Distribute output spectrum in n values")
+	sampleRate := flag.Uint("r", 44100, "Sample rate of the raw audio signal")
+	interval := flag.Duration("i", 50*time.Millisecond, "Window size,")
+	ver := flag.Bool("v", false, "Show version and exit")
+	flag.Parse()
+
+	if *ver {
+		log.Println(version)
+		os.Exit(0)
+	}
+
 	err := espectro.ProcessAudio(os.Stdin, os.Stdout, int(*sampleRate), *interval, int(*n))
 	if err != nil {
 		log.Fatalln(err)
